6f7eafda7a27689dd8cbbc022aa61ce8b38e37af
dcartwright10 <davidcb94@gmail.com>
2019-04-02 13:58:55 -0500

    c1s11

---
+++ factorial.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n = 7;
+	int cont = 1;
+	for(int i =1; i<=n;i++){
+		cont*= i;
+	}
+	cout<< cont <<endl;
+	return 0;
+	} 
---
+++ producto.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a = 8;
+	float b = 4;
+	float c = a*b;
+	cout<< c <<endl;
+	}
